b7bd4bfdca0dd4faaab7b73027b9d599fe0477d3
Nearme7194 <nearmetechteam@gmail.com>
2023-01-28 23:56:36 +0530

    Controller function created

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    //
+    public function index()
+    {
+        return Products::all();
+    }
+
+    public function create()
+    {
+        return 'create';
+    }
+
+    public function store()
+    {
+        return 'store';
+    }
+
+    public function show()
+    {
+        return 'show';
+    }
+
+   public function edit()
+   {
+        return 'edit';
+   }
+
+   public function update()
+   {
+      return 'update';
+   }
+   
+   public function destory()
+   {
+      return 'destory';
+   }
+   
 }
